d721dfbc5a6f9241eaeee3de6ee2c97f0520465e
Seth Tunstall <sethp@graphene.unipart.digital>
2019-12-20 16:09:13 +0000

    lemonldap: WIP

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,4 +17,8 @@ class lemonldap::params (
   $webserver_conf     = [ 'handler', 'manager', 'portal', 'test' ]
   $webserver_prefixes = [ 'reload', 'manager', 'auth', 'test1' ]
   $llng_dir           = '/usr/share/lemonldap-ng'
+  $ldap_host          = lookup('ldap_server')
+  $ldap_base_dn       = lookup('ldap_base_dn')
+  $ldap_user          = lookup('serviceuser')
+  $ldap_password      = lookup('ldap_serviceuser')
 }

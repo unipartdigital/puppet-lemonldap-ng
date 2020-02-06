96de8c1d48e8615b844f6ebe4d95b03cd9861791
Ash Crosby <tcrosby@gmail.com>
2020-02-06 14:18:14 +0000

    Tidy up package install

--- manifests/server/operatingsystem/redhat.pp
+++ manifests/server/operatingsystem/redhat.pp
@@ -2,41 +2,57 @@ class lemonldap::server::operatingsystem::redhat(
   String $sessionstore = 'File',
   String $webserver    = 'apache',
 ) {
-    $gpg_pubkey_id     = $lemonldap::params::gpg_pubkey_id
-    case $sessionstore {
-      /^[Mm]y[Ss][Qq][Ll]$/: {
-        $packagesessions = [ 'mariadb-libs' ]
-      }
-      /^[Pp]ostgre([Ss][Qq][Ll]|s)$/: {
-        $packagesessions = [ 'postgresql-libs' ]
-      }
-      /^[Rr]edis$/: {
-        $packagesessions = [ 'redis' ]
-      }
-      default: {
-        $packagesessions = false
-      }
+    $gpg_pubkey_id = $lemonldap::params::gpg_pubkey_id
+
+    package { [
+        'perl-LWP-Protocol-https',
+        'lasso',
+        'lasso-perl',
+        'perl-Glib',
+        'perl-XML-Simple'
+      ]:
+      ensure => present,
     }
+
+    # case $sessionstore {
+    #   /^[Mm]y[Ss][Qq][Ll]$/: {
+    #     $packagesessions = [ 'mariadb-libs' ]
+    #   }
+    #   /^[Pp]ostgre([Ss][Qq][Ll]|s)$/: {
+    #     $packagesessions = [ 'postgresql-libs' ]
+    #   }
+    #   /^[Rr]edis$/: {
+    #     $packagesessions = [ 'redis' ]
+    #   }
+    #   default: {
+    #     $packagesessions = false
+    #   }
+    # }
+
     case $webserver {
       'nginx': {
-        $packageswebserver = [ 'nginx', 'lemonldap-ng-fastcgi-server', 'perl-LWP-Protocol-https', 'lasso', 'perl-Glib', 'perl-XML-Simple' ]
+        package { [
+            'nginx',
+            'lemonldap-ng-fastcgi-server'
+          ]:
+          ensure  => present,
+        }
       }
       'apache', 'httpd': {
-        $packageswebserver = [ 'httpd', 'mod_perl', 'mod_fcgid', 'perl-LWP-Protocol-https', 'lasso', 'lasso-perl', 'perl-Glib', 'perl-XML-Simple' ]
+        package { [
+            'httpd',
+            'mod_perl',
+            'mod_fcgid'
+          ]:
+          ensure  => present,
+        }
       }
       default: {
         fail("Invalid webserver '${webserver}', please use nginx, apache or httpd")
       }
     }
 
-    if $sessionstore {
-      notify { 'Session store DB config goes here': }
-    }
-
-    if $packagewebserver {
-      package {
-        $packageswebserver:
-          ensure  => present,
-      }
-    }
+    # if $sessionstore {
+    #   notify { 'Session store DB config goes here': }
+    # }
   }

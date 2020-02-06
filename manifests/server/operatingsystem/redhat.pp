9959818d6e3f9f3740216293ac3c913c6b06ab8b
Seth Tunstall <sethp@graphene.unipart.digital>
2020-02-06 14:55:45 +0000

    corrections

--- manifests/server/operatingsystem/redhat.pp
+++ manifests/server/operatingsystem/redhat.pp
@@ -47,6 +47,7 @@ class lemonldap::server::operatingsystem::redhat(
           ]:
           ensure  => present,
         }
+        $packageswebserver = [ 'httpd', 'mod_perl', 'mod_fcgid', 'perl-LWP-Protocol-https', 'lasso', 'lasso-perl', 'perl-Glib', 'perl-XML-Simple' ]
       }
       default: {
         fail("Invalid webserver '${webserver}', please use nginx, apache or httpd")

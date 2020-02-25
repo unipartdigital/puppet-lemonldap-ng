9dea0bcdda32d3359aa7d9e230543981123b7fa9
Seth Tunstall <sethp@graphene.unipart.digital>
2020-02-25 18:16:01 +0000

    parameterise more things

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,8 +11,8 @@
 #
 
 class lemonldap::params(
-  String $domain                 = 'site.example.com' # Override with hiera
-  String $maildomain             = 'example.com' # Override with hiera
+  String $domain                 = 'site.example.com', # Override with hiera
+  String $maildomain             = 'example.com', # Override with hiera
   String $gpg_pubkey_id          = '81F18E7A',
   $webserver_conf                = [ 'handler', 'manager', 'portal', 'test' ],
   $webserver_prefixes            = [ 'reload', 'manager', 'auth', 'test1' ],

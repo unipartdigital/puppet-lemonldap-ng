66596c9fb6935ae769f735578b693c258ec4baf8
Seth Tunstall <sethp@graphene.unipart.digital>
2020-03-16 17:04:36 +0000

    initial augeas config for lmConf

--- manifests/config/augeas.pp
+++ manifests/config/augeas.pp
@@ -1,60 +1,60 @@
 # Build the lmConf-1 config using augeas
 class lemonldap::config::json(
   $json = undef, # hieradata here
+
 ){
-  $context = '/var/lib/lemonldap-ng/conf/test.json'
+  # Pull this lot in wholesale. Remove later when this becomes
+  # lemonldap::config
+  $authldapfilter         = $lemonldap::config::authldapfilter
+  $maildomain             = $lemonldap::config::maildomain
+  $domain                 = $lemonldap::config::domain
+  $timestamp              = $lemonldap::config::timestamp
+  $config_num             = $lemonldap::config::config_num
+  $domain                 = $lemonldap::config::domain
+  $lemon_ldap_key         = $lemonldap::config::lemon_ldap_key
+  $ldap_base_dn           = $lemonldap::config::ldap_base_dn
+  $ldap_group_attribute   = $lemonldap::config::ldap_group_attribute
+  $ldap_group_base        = $lemonldap::config::ldap_group_base
+  $ldap_group_objectclass = $lemonldap::config::ldap_group_objectclass
+  $ldap_port              = $lemonldap::config::ldap_port
+  $ldap_server            = $lemonldap::config::ldap_server
+  $manager_dn             = $lemonldap::config::manager_dn
+  $logo                   = $lemonldap::config::logo
+  $saml_enc_key           = $lemonldap::config::saml_enc_key
+  $saml_sig_key           = $lemonldap::config::saml_sig_key
+  $saml_enc_key_pub       = $lemonldap::config::saml_enc_key_pub
+  $saml_sig_key_pub       = $lemonldap::config::saml_sig_key_pub
+
+  $context = '/files/var/lib/lemonldap-ng/conf/test.json'
 
   augeas { $context:
-    lens => 'Json.lns',
-    incl => $context,
+    lens    => 'Json.lns',
+    changes => [
+      "set AuthLDAPFilter = $authldapfilter",
+      "set certificateResetByMailReplyTo = noreply@${maildomain}",
+      "set certificateResetByMailSender = noreply@${maildomain}",
+      "set certificateResetByMailURL = http://auth.${domain}/certificateReset",
+      "set cfgDate = $timestamp",
+      "set cfgNum = $config_num",
+      "set domain = $domain",
+      "set key/$lemon_ldap_key",
+      "set ldapBase = $ldap_base_dn",
+      "set ldapGroupAttributeName = $ldap_group_attribute",
+      "set ldapGroupBase = $ldap_group_base",
+      "set ldapGroupObjectClass = $ldap_group_objectclass",
+      "set ldapPort = $ldap_port",
+      "set ldapServer = $ldap_server",
+      "set mailFrom = noreply@${maildomain}",
+      "set mailUrl = https://auth.${domain}/resetpwd",
+      "set managerDn = $manager_dn",
+      "set managerPassword = $manager_password",
+      "set portal = https://auth.${domain}",
+      "set portalMainLogo = $logo",
+      "set registerUrl = https://auth.${domain}/register",
+      "set samlServicePrivateKeyEnc = $saml_enc_key",
+      "set samlServicePrivateKeySig = $saml_sig_key",
+      "set samlServicePublicKeyEnc = $saml_enc_key_pub",
+      "set samlServicePublicKeySig = $saml_sig_key_pub",
+    ]
   }
-
-  #"AuthLDAPFilter": "<%= @authldapfilter %>",
-  #            "catname": "<%= @company %>",
-  #                    "uri": "https://manager.<%= @domain %>/manager.html"
-  #                    "uri": "https://manager.<%= @domain %>/notifications.html"
-  #                    "uri": "https://manager.<%= @domain %>/sessions.html"
-  #                    "uri": "https://manager.<%= @domain %>/doc/"
-  #    "certificateResetByMailReplyTo": "noreply@<%= @maildomain %>",
-  #    "certificateResetByMailSender": "noreply@<%= @maildomain %>",
-  #    "certificateResetByMailURL": "http://auth.<%= @domain %>/certificateReset",
-  #    "cfgDate": <%= @timestamp %>,
-  #    "cfgNum": <%= @config_num %>,
-  #    "domain": "<%= @domain %>",
-  #        "auth.<%= @domain %>": {
-  #        "manager.<%= @domain %>": {
-  #        "test1.<%= @domain %>": {
-  #    "key": "<%= @lemon_ldap_key %>",
-  #    "ldapBase": "<%= @ldap_base_dn %>",
-  #    "ldapGroupAttributeName": "<%= @ldap_group_attribute %>",
-  #    "ldapGroupBase": "<%= @ldap_group_base %>",
-  #    "ldapGroupObjectClass": "<%= @ldap_group_objectclass %>",
-  #    "ldapPort": <%= @ldap_port %>,
-  #    "ldapServer": "<%= @ldap_server %>",
-  #        "auth.<%= @domain %>": {
-  #       "(?#checkUser)^/checkuser": "groupMatch($hGroups, 'cn', '<%= @ldap_admin_group %>')",
-  #        "manager.<%= @domain %>": {
-  #            "(?#Notifications)/(.*?\\.(fcgi|psgi)/)?notifications": "groupMatch($hGroups, 'cn', '<%= @ldap_admin_group %>')",
-  #            "(?#Sessions)/(.*?\\.(fcgi|psgi)/)?sessions": "groupMatch($hGroups, 'cn', '<%= @ldap_admin_group %>')",
-  #            "default": "groupMatch($hGroups, 'cn', '<%= @ldap_admin_group %>')"
-  #        "test1.<%= @domain %>": {
-  #            "default": "groupMatch($hGroups, 'cn', '<%= @ldap_admin_group %>')"
-  #    "mailFrom": "noreply@<%= @maildomain %>",
-  #    "mailUrl": "https://auth.<%= @domain %>/resetpwd",
-  #    "managerDn": "<%= @manager_dn %>",
-  #    "managerPassword": "<%= @manager_password %>",
-  #    "portal": "https://auth.<%= @domain %>/",
-  #    "portalMainLogo": "<%= @logo %>",
-  #        "auth.<%= @domain %>": {
-  #     "manager.<%= @domain %>": {
-  #        "test1.<%= @domain %>": {
-  #    "registerUrl": "https://auth.<%= @domain %>/register",
-  #        "localhost": "https://reload.<%= @domain %>/reload"
-  #    "samlServicePrivateKeyEnc": "<%= @saml_enc_key %>",
-  #    "samlServicePrivateKeySig": "<%= @saml_sig_key %>",
-  #    "samlServicePublicKeyEnc": "<%= @saml_enc_key_pub %>",
-  #    "samlServicePublicKeySig": "<%= @saml_sig_key_pub %>",
-  #        "auth.<%= @domain %>": {
-  #        "manager.<%= @domain %>": {
-  ##        "test1.<%= @domain %>": {
-  }
+}
